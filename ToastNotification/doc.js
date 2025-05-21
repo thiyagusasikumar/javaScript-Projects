9957b9c239393997c7fe09d0ffd88554f1b4c5c0
Thiyagu S <sthiyagusasikumark@gmail.com>
2025-05-21 15:13:01 +0530

    added task related to toast notification

---
+++ ToastNotification/doc.js
@@ -0,0 +1,26 @@
+let completed=document.getElementById("completed");
+let error=document.getElementById("error");
+let invaild=document.getElementById("invalid");
+let notification=document.querySelector(".notification");
+completed.onclick=()=>{
+    console.log("working");
+    let newElement=document.createElement('div');
+    newElement.className = 'tinos-bold ToastBox';
+    newElement.innerHTML=' <p><i class="fa-solid fa-check iconComplete"></i> Task Was Completed</p>';
+    notification.appendChild(newElement);
+    setTimeout(()=>{
+        newElement.remove;
+    },6000);
+};
+error.onclick=()=>{
+    let newElement=document.createElement('div');
+    newElement.className = 'tinos-bold ToastBox';
+    newElement.innerHTML=' <p><i class="fa-solid fa-xmark iconError"></i> Error In The Task</p>';
+    notification.appendChild(newElement);
+};
+invaild.onclick=()=>{
+    let newElement=document.createElement('div');
+    newElement.className = 'tinos-bold ToastBox';
+    newElement.innerHTML='<p><i class="fa-solid fa-exclamation iconInvaild"></i> Invaild Option</p>';
+    notification.appendChild(newElement);
+};
